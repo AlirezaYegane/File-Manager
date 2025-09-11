cd3082388230c44522c0f37c97932ce94b43d1fa
AlirezaYegane <alireza.yegane.cs@gmail.com>
2025-09-11 21:10:09 +0330

    main updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,32 @@
 package org.example;
+
 import org.example.auth.repository.FileUserRepository;
 import org.example.auth.repository.UserRepository;
 import org.example.auth.service.UserService;
 import org.example.auth.service.UserServiceImpl;
 import org.example.auth.ui.Menu;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        UserRepository userRepository = new FileUserRepository("users.txt");
+        Path desktop = Paths.get(System.getProperty("user.home"), "Desktop");
+
+        Path dataFile = desktop.resolve("users.json");
+
+        try {
+            Files.createDirectories(dataFile.getParent());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        UserRepository userRepository = new FileUserRepository(dataFile.toString());
         UserService userService = new UserServiceImpl(userRepository);
         Menu menu = new Menu(userService);
+
+        System.out.println("Data file path: " + dataFile);
         menu.start();
     }
 }
