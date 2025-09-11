77e3de214b8461a415c58af5847cf1923b9c4a7b
AlirezaYegane <alireza.yegane.cs@gmail.com>
2025-09-11 21:48:22 +0330

    main updated

--- src/main/java/org/example/auth/model/User.java
+++ src/main/java/org/example/auth/model/User.java
@@ -4,13 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @lombok.ToString(exclude = "password")
-public class User implements Serializable {
+public class User {
     private String username;
     private String password;
 }
