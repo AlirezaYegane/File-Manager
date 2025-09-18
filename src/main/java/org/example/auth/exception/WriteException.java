fc6d64dd415fdb0c388ca7dccefe7844011e0ef7
AlirezaYegane <alireza.yegane.cs@gmail.com>
2025-09-19 00:27:03 +0400

    WriteException edit in class

---
+++ src/main/java/org/example/auth/exception/WriteException.java
@@ -0,0 +1,18 @@
+package org.example.auth.exception;
+
+public class WriteException extends RuntimeException{
+    public WriteException() {
+    }
+
+    public WriteException(String message) {
+        super(message);
+    }
+
+    public WriteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WriteException(Throwable cause) {
+        super(cause);
+    }
+}
