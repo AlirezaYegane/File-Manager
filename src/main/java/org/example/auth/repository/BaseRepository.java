d1617a2f7ace555b70e50399096f2ebf626313ef
AlirezaYegane <alireza.yegane.cs@gmail.com>
2025-09-19 00:25:00 +0400

    BaseRepository edit in class

---
+++ src/main/java/org/example/auth/repository/BaseRepository.java
@@ -0,0 +1,12 @@
+//package org.example.auth.repository;
+//
+//import org.example.auth.model.User;
+//
+//import java.util.Map;
+//
+//public abstract class BaseRepository {
+//    private Map<String, User> loadUsersFromFile(String path) {
+//
+//    }
+//
+//    }
